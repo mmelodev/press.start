3c45032f7eacace5e8a3a8e3d7d0ae47d981b700
Murilo Melo <135653390+mmelodev@users.noreply.github.com>
2024-02-06 05:58:54 +0000

    adiciona a pasta pages

---
+++ pages/index.js
@@ -0,0 +1,7 @@
+function Home(){
+    return <h1>
+        Espero que você tenha um dia excelente, você é uma pessoa incrível! Sempre estarei aqui, caso precise! Amo você!
+    </h1>
+}
+
+export default Home;
